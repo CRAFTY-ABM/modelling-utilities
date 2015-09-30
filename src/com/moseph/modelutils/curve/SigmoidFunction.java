5d0ffb5431bf08920c490a53c9f8cab4fbe22b3b
S-Holzhauer <S-Holzhauer@localhost>
2015-09-30 12:46:30 +0100

    :: Modelling Utilities :: 0.5.5 - medium changes in SigmoidFunction: added horizontal and (normalised) vertical shifting

--- src/com/moseph/modelutils/curve/SigmoidFunction.java
+++ src/com/moseph/modelutils/curve/SigmoidFunction.java
@@ -4,9 +4,12 @@ import org.simpleframework.xml.Attribute;
 
 
 /**
- * Implements:
+ * Implements (not considering normalisation):
  * 
- * {a*x^p/(h^p + abs(x^p))}
+ * {a*(x-c)^p/(h^p + abs((x-c)^p)) + d}
+ * 
+ * LaTeX: {a(x-c)^p} \over {(h^p + |(x-c)^p|)} + d
+ * 
  * 
  * @author dmrust
  * 
@@ -22,6 +25,15 @@ public class SigmoidFunction implements Curve
 	// power - controls steepness (defaults to 3, note that even numbers result in values all >= 0)
 	@Attribute( required=false)
 	double P = 3;
+	// horizontal shift (defaults to 0)
+	@Attribute(required = false)
+	double C = 0;
+	// vertical shift (defaults to 0)
+	@Attribute(required = false)
+	double D = 0;
+	// relevant when D != 0: scales values to [-1,1] while keeping the turning point at level d
+	@Attribute(required = false)
+	boolean normalise = true;
 	
 	/**
 	 * Simplest constructor.
@@ -29,13 +41,20 @@ public class SigmoidFunction implements Curve
 	 * @param A
 	 * @param H
 	 * @param P
+	 * @param C
+	 * @param D
+	 * @param normalise
 	 */
 	public SigmoidFunction(@Attribute(name = "A") double A, @Attribute(name = "H") double H,
-			@Attribute(name = "P") double P)
+			@Attribute(name = "P") double P, @Attribute(name = "C") double C, @Attribute(name = "D") double D,
+			boolean normalise)
 	{
 		this.A = A;
 		this.H = H;
 		this.P = P;
+		this.C = C;
+		this.D = D;
+		this.normalise = normalise;
 	}
 
 	/**
@@ -43,6 +62,11 @@ public class SigmoidFunction implements Curve
 	 */
 	public double sample( double position )
 	{
-		return A * Math.pow(position, P) / (Math.pow(H, P) + Math.abs(Math.pow(position, P)));
+		double f = A * Math.pow(position - C, P) / (Math.pow(H, P) + Math.abs(Math.pow(position - C, P))) + D;
+
+		if (D != 0 && normalise) {
+			f = f < D ? (D + (f - D) * (1.0 + D)) : (D + (f - D) * (1.0 - D));
+		}
+		return f;
 	}
 }
